1a856b095899a579e2976db40b8ee5f6ef0ec722
ENiGMA-101 <hamdilhasan200@gmail.com>
2025-01-22 09:08:06 +0600

    ref

---
+++ C++ Lang/Tutorial/reference.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    double a[n];
+    for(auto &i : a)
+        cin>> i;
+    for(auto i : a)
+        cout<< i << " ";
+}
